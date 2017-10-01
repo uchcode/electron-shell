a1e6e9c3e17be4c5df682cabbfbd2dc3fb66b9f9
Tom.U <tom@tom-macmini.local>
2017-10-01 13:27:06 +0900

    changed bundle extension

--- examples/003_bundle_demo.electronhtmld/index.js
+++
@@ -1,3 +0,0 @@
-document.log(`window.show: ${!!window.show}`)
-document.log(`window.state: ${!!window.state}`)
-document.log(`document.log: ${!!document.log}`)
---
+++ examples/003_bundle_demo.htmld/index.js
@@ -0,0 +1,3 @@
+document.log(`window.show: ${typeof window.show}`)
+document.log(`window.state: ${typeof window.state}`)
+document.log(`document.log: ${typeof document.log}`)
