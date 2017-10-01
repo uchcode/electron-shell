7e1dcd57cf9fb87778497b9e2970babce9382ba3
Tom.U <tom@tom-macmini.local>
2017-10-01 13:25:22 +0900

    ver from 1.7.6 to 1.8.1

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-electron-packager sources "Electron Shell" --platform=darwin -arch=x64 --electronVersion=1.7.6 --app-bundle-id=io.github.uchcode.electron-shell --extend-info=resources/info.plist --extra-resource=resources/ElectronHTML.icns
+electron-packager sources "Electron Shell" --platform=darwin -arch=x64 --electronVersion=1.8.1 --app-bundle-id=io.github.uchcode.electron-shell --extend-info=resources/info.plist --extra-resource=resources/ElectronHTML.icns
